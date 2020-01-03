a9ea79dd87e4fe24b0ef597b6dee1f9af5680294
Brad Candell <bcandell@pesi.com>
2020-01-03 05:50:48 -0600

    Added a numbered list to the Join page so I can see how the page looks with longer content. I suspect that I will need to re-work some of the overall styles because the CSS variables aren't producing good enough results, and these are public websites so I need to consider backwards compatibility

--- src/views/Join/Join.tsx
+++ src/views/Join/Join.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 export const Join: React.FC = () => {
-    return <h1>Join</h1>;
+    const generateItems = () => {
+        const results: Array<React.ReactElement> = new Array<React.ReactElement>();
+        for (let i = 1; i < 100; i++) {
+            results.push(<li key={i}>{i}</li>);
+        }
+        return results;
+    };
+
+    return (
+        <>
+            <h1>Join</h1>
+            <ul>{generateItems()}</ul>
+        </>
+    );
 };
 
 export default Join;
